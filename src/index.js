5302c5be214ac59e325eab34202fb2c5edf76824
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-18 11:10:41 +0200

    added info

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
-import {fetchBreeds} from './js/cat-api.js';
+import {fetchBreeds, fetchCatByBreed} from './js/cat-api.js';
 
 const breebsSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 error.style.display = 'none';
+
+const renderBreedInfo = (breed) => {
+    const breedInfo = breed.breeds[0];
+    const infoHTMLElement = `
+        <img src="${breed.url}" width="600" height="600" style="object-fit: contain;"/>
+        <div>
+            <h2>${breedInfo.name}</h2>
+            <p>${breedInfo.description}</p>
+            <ul>${breedInfo.temperament.split(', ').map((it) => `<li>${it}</li>`).join('')}</ul>
+        </div>
+    `;
+    catInfo.innerHTML = '';
+    catInfo.insertAdjacentHTML('afterbegin', infoHTMLElement)
+}
+
+const onBreedsSelectChange = async (e) => {
+    const chosenBreed = e.target[breebsSelect.selectedIndex];
+
+    try {
+        loader.style.display = 'block';
+        const resp = await fetchCatByBreed(chosenBreed.value);
+        renderBreedInfo(resp[0]);
+        loader.style.display = 'none';
+    } catch (err) {
+        loader.style.display = 'none';
+        error.style.display = 'block';
+        console.error('Cannot render breed info. Error occured: ', err);
+    }
+};
 
 const main = async () => {
     try {
         const breeds = await fetchBreeds();
         const options = breeds.map((b) => `<option value="${b.id}">${b.name}</option>`).join('');
-        breebsSelect.insertAdjacentHTML('afterbegin', options)
+        breebsSelect.insertAdjacentHTML('afterbegin', options);
+        breebsSelect.addEventListener('change', onBreedsSelectChange);
         loader.style.display = 'none';
     } catch (err) {
         loader.style.display = 'none';
         error.style.display = 'block';
+        console.error('Cannot render breeds. Error occured: ', err);
     }
+};
 
-}
-
-main()
+main();
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,9 +5,19 @@ axios.defaults.headers.common["x-api-key"] = 'live_HiesbHCNozFabauA2TlzO2OtiyWGe
 export const fetchBreeds = async () => {
     try {
         const resp = await axios.get('https://api.thecatapi.com/v1/breeds');
+
         return resp.data; 
     } catch (err) {
-        console.error(err);
-        return [];
+        console.error('Cannot fetch breeds. Error occured: ', err);
+    }
+}
+
+export const fetchCatByBreed = async (id) => {
+    try {
+        const resp = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`);
+
+        return resp.data; 
+    } catch (err) {
+        console.error('Cannot fetch breed info. Error occured: ', err);
     }
 }
